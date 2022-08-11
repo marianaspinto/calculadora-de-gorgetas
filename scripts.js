b69e64ad03e9bd9637dae245cb554fa85f4f878e
Mariana Pinto <mariana.vendasbb@gmail.com>
2022-08-11 12:57:43 -0300

    js code correction

--- scripts.js
+++ scripts.js
@@ -1,32 +1,28 @@
 function calculateTip(event) {
-    event.preventDeDefault();
-    console.log(event);
-   
-    let bill = document.getElementById('bill').value;
-    let serviceQual = document.getElementById(serviceQual).value;
-    let numOfPeople = document.getElementById(people).value;
+ event.preventDefault();
+ let bill = document.getElementById('bill').value;
+ let serviceQual = document.getElementById('serviceQual').value;
+ let numOfPeople = document.getElementById('people').value;
 
-    if(bill == '' | serviceQual == 0){
-        alert('Por favor preencha os valores')
-        return;
-    }
+ if(bill == '' | serviceQual == 0) {
+    alert('Por favor, preencha os valores')
+    return;
+ }
 
-    if(numPeople == '' | numPeople <= 1) {
-        numPeople = 1;
-        document.getElementById('each').style.dysplay = 'none'
-        else {
-        document.getElementById('each').style.display = 'block'
-        
-    }
+ if(numOfPeople == '' | numOfPeople <= 1) {
+    numOfPeople =1;
+    document.getElementById('each').style.display = "none";
+ } else {
+    document.getElementById('each').style.display = "block";
+ }
 
-    let total = (bill * serviceQual) / numPeople;
-    total = total.toFixed(2);
-    document.getElementById('tip').innerHTML = total;
-    document.getElementById('totalTip').style.display = "block";
-   
+ let total = (bill * serviceQual) / numOfPeople;
+ total = total.toFixed(2);
+ document.getElementById('tip').innerHTML = total;
+ document.getElementById('totalTip').style.display = "block";
 }
 
 document.getElementById('totalTip').style.display = "none";
 document.getElementById('each').style.display = "none";
 
-Document.getElementById('tipsForm').addEventListener('submit', calculateTip);
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
